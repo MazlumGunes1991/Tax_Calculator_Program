bfda7b02b2ad372bc75536773efd389acb7e2a81
Mazlum Güneş <gunesmazlum1991@gmail.com>
2023-03-18 15:57:01 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        double priceWithoutTax,priceWithTax,priceTax;
+
+        System.out.print("Your amount: ");
+        priceWithoutTax = input.nextDouble();
+
+        priceWithTax = priceWithoutTax + ((priceWithoutTax * 18) /100) ;
+        System.out.println("Amount with Tax: " + priceWithTax);
+
+        priceTax = (priceWithoutTax * 18) / 100;
+        System.out.print("Tax Amount: " + priceTax);
     }
 }
